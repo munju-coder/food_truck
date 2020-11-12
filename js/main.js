df346855cad62973c84c93f84b79646126c05ed1
munjucho-corder <dailymunju@naver.com>
2020-11-12 14:24:04 +0900

    20201112

--- js/main.js
+++ js/main.js
@@ -21,7 +21,14 @@ $(document).ready( function(){
     /**
      * @bref menu click event
      */
-    $('#menu_btn,.close_btn').click( function(){
-        $('aside,.modal_background').toggle();
+    $('#menu_btn').click( function(){
+        // $('aside,.modal_background').toggle();
+        $('.modal_background').toggle();
+        $('aside').css('right','0');
+    });
+
+    $('.close_btn,.modal_background').click( function(){
+        $('.modal_background').toggle();
+        $('aside').css('right','-520px');
     });
 });
